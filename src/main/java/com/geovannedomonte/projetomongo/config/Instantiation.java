2c9fdb18499c3071e7adedc521810389d41b05b6
Geovanne do Monte <geovannedomonte@gmail.com>
2025-02-13 20:26:08 -0300

    Operação de instanciação da base de dados

---
+++ src/main/java/com/geovannedomonte/projetomongo/config/Instantiation.java
@@ -0,0 +1,33 @@
+package com.geovannedomonte.projetomongo.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import com.geovannedomonte.projetomongo.domain.User;
+import com.geovannedomonte.projetomongo.repository.UserRepository;
+
+@Configuration
+public class Instantiation implements CommandLineRunner{
+
+	@Autowired
+	private UserRepository userRepository;
+	
+	@Override
+	public void run(String... args) throws Exception {
+		
+		
+		userRepository.deleteAll();
+		
+		User maria = new User(null, "Maria Gomes Silva", "maria@gmail.com");
+		User alex = new User(null, "Alex DLuto", "dluto@gmail.com");
+		User geo = new User(null, "Geo do Monyte", "gg@gmail.com");
+		
+		userRepository.saveAll(Arrays.asList(maria, alex, geo));
+	}
+
+	
+
+}
