b560eae79b140873a5f412b140d22ac20359d9a5
Geovanne do Monte <geovannedomonte@gmail.com>
2025-02-25 20:26:27 -0300

    Obtendo um post por id

---
+++ src/main/java/com/geovannedomonte/projetomongo/services/PostService.java
@@ -0,0 +1,25 @@
+package com.geovannedomonte.projetomongo.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.geovannedomonte.projetomongo.domain.Post;
+import com.geovannedomonte.projetomongo.repository.PostRepository;
+import com.geovannedomonte.projetomongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository reposi;
+	
+	
+	public Post findById(String id) {
+			Post user = reposi.findById(id).orElse(null);
+			if (user == null) {
+				throw new ObjectNotFoundException("Obejeto não encintrado");
+			}
+			return user;
+	}
+	
+}
