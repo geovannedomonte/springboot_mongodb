0ca28e12e2ff93fa52ed7f6b81af298954fa5710
Geovanne do Monte <geovannedomonte@gmail.com>
2025-02-25 19:31:30 -0300

    Projeção dos dados do autor com DTO

--- src/main/java/com/geovannedomonte/projetomongo/config/Instantiation.java
+++ src/main/java/com/geovannedomonte/projetomongo/config/Instantiation.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.geovannedomonte.projetomongo.domain.Post;
 import com.geovannedomonte.projetomongo.domain.User;
+import com.geovannedomonte.projetomongo.dto.AuthorDTO;
 import com.geovannedomonte.projetomongo.repository.PostRepository;
 import com.geovannedomonte.projetomongo.repository.UserRepository;
 
@@ -35,11 +36,12 @@ public class Instantiation implements CommandLineRunner{
 		User alex = new User(null, "Alex Ta DLuto", "dluto@gmail.com");
 		User geo = new User(null, "Geo Super Monte", "gg@gmail.com");
 		
-		Post post1 = new Post(null, sdf.parse("01/01/2025"), "Feliz Ano Novo!", "Muita saude e paz para esse ano.", maria);
-		Post post2 = new Post(null, sdf.parse("01/01/2025"), "Primeira postagem do ano!", "Muitas Felicidades minha gente.", maria);
-		
-		
 		userRepository.saveAll(Arrays.asList(maria, alex, geo));
+		
+		Post post1 = new Post(null, sdf.parse("01/01/2025"), "Feliz Ano Novo!!!!!", "Muita saude e paz para esse ano.", new AuthorDTO(maria));
+		Post post2 = new Post(null, sdf.parse("01/01/2025"), "Primeira postagem do ano!!!!!", "Muitas Felicidades minha gente.", new AuthorDTO(maria));
+		
+		
 		postRepository.saveAll(Arrays.asList(post1, post2));
 	}
 
