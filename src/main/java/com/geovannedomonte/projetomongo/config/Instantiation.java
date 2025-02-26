5576b70b2d9c81c812494033649c33537ecf7994
Geovanne do Monte <geovannedomonte@gmail.com>
2025-02-25 21:21:24 -0300

    Acrescentando comentários aos posts

--- src/main/java/com/geovannedomonte/projetomongo/config/Instantiation.java
+++ src/main/java/com/geovannedomonte/projetomongo/config/Instantiation.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 import com.geovannedomonte.projetomongo.domain.Post;
 import com.geovannedomonte.projetomongo.domain.User;
 import com.geovannedomonte.projetomongo.dto.AuthorDTO;
+import com.geovannedomonte.projetomongo.dto.CommentDTO;
 import com.geovannedomonte.projetomongo.repository.PostRepository;
 import com.geovannedomonte.projetomongo.repository.UserRepository;
 
@@ -41,6 +42,12 @@ public class Instantiation implements CommandLineRunner{
 		Post post1 = new Post(null, sdf.parse("01/01/2025"), "Feliz Ano Novo!!!!!", "Muita saude e paz para esse ano.", new AuthorDTO(maria));
 		Post post2 = new Post(null, sdf.parse("01/01/2025"), "Primeira postagem do ano!!!!!", "Muitas Felicidades minha gente.", new AuthorDTO(maria));
 		
+		CommentDTO c1 = new CommentDTO("Feliz ano novo minha amiga", sdf.parse("02/01/2025"), new AuthorDTO(alex));
+		CommentDTO c2 = new CommentDTO("FAZ o L", sdf.parse("02/01/2025"), new AuthorDTO(geo));
+		CommentDTO c3 = new CommentDTO("Viciada em internet", sdf.parse("03/01/2025"), new AuthorDTO(alex));
+		
+		post1.getComments().addAll(Arrays.asList(c1, c2));
+		post2.getComments().addAll(Arrays.asList(c3));
 		
 		postRepository.saveAll(Arrays.asList(post1, post2));
 		
